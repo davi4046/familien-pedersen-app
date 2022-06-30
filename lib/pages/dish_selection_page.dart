4329e8fef42ec4e38831395cccf2c2de3e9ce74a
David Rud Pedersen <davidrudpedersen@gmail.com>
2022-06-30 18:03:34 +0200

    dish selection page has been created.

---
+++ lib/pages/dish_selection_page.dart
@@ -0,0 +1,306 @@
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:expandable/expandable.dart';
+import 'foodplan_creation_page.dart';
+import 'package:flutter/material.dart';
+
+final List<Dish> _dishes = [
+  Dish(title: 'Spaghetti med kødssovs', time: '45 min'),
+  Dish(title: 'Burger', time: '30 min'),
+  Dish(title: 'Spejdergryde', time: '50 min'),
+];
+
+class DishSelectionPage extends StatelessWidget {
+  const DishSelectionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Vælg en ret'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.filter_alt),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: _dishes.map((dish) {
+          return DishListItem(dish: dish);
+        }).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
+
+class DishListItem extends StatefulWidget {
+  final Dish dish;
+
+  DishListItem({
+    Key? key,
+    required this.dish,
+  }) : super(key: key);
+
+  @override
+  State<DishListItem> createState() => _DishListItemState();
+}
+
+class _DishListItemState extends State<DishListItem> {
+  final _controller = ExpandableController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        InkWell(
+          onTap: () => Navigator.pop(context, widget.dish),
+          child: Container(
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16, top: 16, right: 16),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Flexible(
+                              child: Text(
+                                widget.dish.title,
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                            Text(
+                              widget.dish.time,
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        if (!_controller.expanded) ...[
+                          RatingBarIndicator(
+                            rating: 2.75,
+                            itemBuilder: (context, index) => const Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                            itemCount: 5,
+                            itemSize: 24,
+                          ),
+                        ],
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 125,
+                  height: double.infinity,
+                  child: Image.network(
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          decoration: const BoxDecoration(
+            border: Border(bottom: BorderSide(width: 1)),
+          ),
+          child: ExpandablePanel(
+            controller: _controller
+              ..addListener(() {
+                setState(() {});
+              }),
+            collapsed: SizedBox(
+              width: double.infinity,
+              child: ExpandableButton(
+                child: const Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text('Vis mere'),
+                  ),
+                ),
+              ),
+            ),
+            expanded: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: Row(
+                          children: [
+                            Container(
+                              width: 40,
+                              height: 40,
+                              alignment: Alignment.center,
+                              child: Text(
+                                  'Alle',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8),
+                              child: RatingBarIndicator(
+                                rating: 2.75,
+                                itemBuilder: (context, index) => const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                itemCount: 5,
+                                itemSize: 24,
+                              ),
+                            ),
+                            Text(
+                              '(4)',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              child: ClipOval(
+                                child: Image.network('https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541'),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8),
+                              child: RatingBarIndicator(
+                                rating: 2.75,
+                                itemBuilder: (context, index) => const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                itemCount: 5,
+                                itemSize: 24,
+                              ),
+                            ),
+                            Text(
+                              '(2)',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              child: ClipOval(
+                                child: Image.network('https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541'),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8),
+                              child: RatingBarIndicator(
+                                rating: 2.75,
+                                itemBuilder: (context, index) => const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                itemCount: 5,
+                                itemSize: 24,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              child: ClipOval(
+                                child: Image.network('https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541'),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8),
+                              child: RatingBarIndicator(
+                                rating: 2.75,
+                                itemBuilder: (context, index) => const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                itemCount: 5,
+                                itemSize: 24,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                                'Antal gange serveret:',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              '3',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Sidst serveret:',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            Text(
+                              '2 måneder siden',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: ExpandableButton(
+                    child: const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text('Vis mindre'),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            theme: const ExpandableThemeData(hasIcon: false),
+          ),
+        ),
+      ],
+    );
+  }
+}
